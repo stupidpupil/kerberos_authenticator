19657788102fca4855e022585eda57e80027af61
Adam Watkins <adam@stupidpupil.co.uk>
2016-04-09 19:30:03 +0100

    Try different libraries

--- run_integration_test.sh
+++ run_integration_test.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 
-source integration_test_support/environment
+ret=0
 
-echo 'Use MacPorts MIT library' 
-FFI_KRB5_LIBRARY_NAME=/opt/local/lib/libkrb5.dylib bacon -a
+if [ -n $KA_SPEC_RUN_INTEGRATION ]; then
+  source integration_test_support/environment
+fi
 
-echo ''
-echo 'Use OS X Heimdal library'
-FFI_KRB5_LIBRARY_NAME=/usr/lib/libkrb5.dylib bacon -a
+run_bacon ( ) {
+  if [ -f $library_path ]; then
+    echo $library_name
+    FFI_KRB5_LIBRARY_NAME=$library_path bacon -a
+    ret=$(($ret+$?))
+    echo ''
+  fi
+}
+
+library_name='MacPorts MIT'
+library_path='/opt/local/lib/libkrb5.dylib'
+run_bacon
+
+library_name='OS X Heimdal'
+library_path='/usr/lib/libkrb5.dylib'
+run_bacon
+
+library_name='Debian Heimdal'
+library_path='/usr/lib/x86_64-linux-gnu/libkrb5.so.26'
+run_bacon
+
+library_name='Debian MIT'
+library_path='/usr/lib/x86_64-linux-gnu/libkrb5.so.3'
+run_bacon
+
+exit $ret
