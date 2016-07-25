54866b0ca90ca0bf04d3cc3c34de9589030f913b
Adam Watkins <adam@stupidpupil.co.uk>
2016-07-25 02:07:42 +0100

    Substitute 'resolve' for 'create'

--- spec/keytab_spec.rb
+++ spec/keytab_spec.rb
@@ -10,20 +10,20 @@ describe KerberosAuthenticator::Krb5::Keytab do
     end
   end
 
-  describe 'when I try to create a Keytab with a type of FILE and a path' do
+  describe 'when I try to resolve a Keytab with a type of FILE and a path' do
     it 'must return a Keytab with that name' do
       KerberosAuthenticator::Krb5::Keytab.new_with_name('FILE:/etc/krb5.keytab').name.should.match /\/etc\/krb5.keytab$/
       #It should EQUAL 'FILE:/etc/krb5.keytab' but not all Kerberos implementations seem to do this.
     end
   end
 
-  describe 'when I try to create a Keytab with a type of FILE and a UTF-8 path' do
+  describe 'when I try to resolve a Keytab with a type of FILE and a UTF-8 path' do
     it 'must return a Keytab with that name' do
       KerberosAuthenticator::Krb5::Keytab.new_with_name('FILE:/итд/krb5.keytab').name.should.match /\/итд\/krb5.keytab$/
     end
   end
 
-  describe 'when I try to create a Keytab with a nonsensical type' do
+  describe 'when I try to resolve a Keytab with a nonsensical type' do
     it 'must raise an Error' do
       -> { KerberosAuthenticator::Krb5::Keytab.new_with_name('DOJO:講道館') }.should.raise KerberosAuthenticator::Krb5::Error
     end
