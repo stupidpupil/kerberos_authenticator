e612f18837be84c97393830b100b52cafe8319ec
Adam Watkins <adam@stupidpupil.co.uk>
2016-07-25 02:02:57 +0100

    Add some Keytab calls and specs to match

--- lib/kerberos_authenticator/krb5/keytab.rb
+++ lib/kerberos_authenticator/krb5/keytab.rb
@@ -3,8 +3,12 @@ module KerberosAuthenticator
     typedef :pointer, :krb5_keytab
 
     attach_function :krb5_kt_resolve, [:krb5_context, :string, :buffer_out], :krb5_error_code
+    attach_function :krb5_kt_default, [:krb5_context, :buffer_out], :krb5_error_code
+
     attach_function :krb5_kt_close, [:krb5_context, :krb5_keytab], :krb5_error_code
+    
     attach_function :krb5_kt_get_type, [:krb5_context, :krb5_keytab], :string
+    attach_function :krb5_kt_get_name, [:krb5_context, :krb5_keytab, :buffer_out, :int], :krb5_error_code
 
     # Storage for locally-stored keys.
     class Keytab
@@ -16,6 +20,15 @@ module KerberosAuthenticator
       def self.new_with_name(name, context = Context.context)
         buffer = FFI::Buffer.new :pointer
         Krb5.kt_resolve(context.ptr, name, buffer)
+
+        new(context, buffer)
+      end
+
+      # @return [Keytab] the default keytab
+      # @see http://web.mit.edu/Kerberos/krb5-1.14/doc/appdev/refs/api/krb5_kt_default.html krb5_kt_default
+      def self.default(context = Context.context)
+        buffer = FFI::Buffer.new :pointer
+        Krb5.kt_default(context.ptr, buffer)
 
         new(context, buffer)
       end
@@ -42,6 +55,17 @@ module KerberosAuthenticator
       # @see http://web.mit.edu/Kerberos/krb5-1.14/doc/appdev/refs/api/krb5_kt_get_type.html kt_get_type
       def type
         Krb5.kt_get_type(context.ptr, ptr)
+      end
+
+      # The maximum length, in bytes, that can be read by #name .
+      GET_NAME_MAX_LENGTH = 512
+
+      # @return [String] the name of the key table
+      # @see http://web.mit.edu/Kerberos/krb5-1.14/doc/appdev/refs/api/krb5_kt_get_name.html kt_get_name
+      def name
+        buffer = FFI::Buffer.new :char, GET_NAME_MAX_LENGTH
+        Krb5.kt_get_name(context.ptr, ptr, buffer, GET_NAME_MAX_LENGTH)
+        buffer.read_bytes(255).force_encoding('UTF-8').split("\x00",2)[0]
       end
 
       # Builds a Proc to close the Keytab once its no longer in use.
---
+++ spec/keytab_spec.rb
@@ -0,0 +1,32 @@
+# Run some basic specs on Krb5::Keytab to check that:
+# - the FFI interface can load the library and call functions
+# - the error handling in the FFI interface is working
+describe KerberosAuthenticator::Krb5::Keytab do
+
+  describe 'when I try to resolve the default Keytab' do
+    it 'must return a Keytab' do
+      a_keytab = lambda { |obj| obj.is_a?(KerberosAuthenticator::Krb5::Keytab) }
+      KerberosAuthenticator::Krb5::Keytab.default.should.be a_keytab
+    end
+  end
+
+  describe 'when I try to create a Keytab with a type of FILE and a path' do
+    it 'must return a Keytab with that name' do
+      KerberosAuthenticator::Krb5::Keytab.new_with_name('FILE:/etc/krb5.keytab').name.should.match /\/etc\/krb5.keytab$/
+      #It should EQUAL 'FILE:/etc/krb5.keytab' but not all Kerberos implementations seem to do this.
+    end
+  end
+
+  describe 'when I try to create a Keytab with a type of FILE and a UTF-8 path' do
+    it 'must return a Keytab with that name' do
+      KerberosAuthenticator::Krb5::Keytab.new_with_name('FILE:/итд/krb5.keytab').name.should.match /\/итд\/krb5.keytab$/
+    end
+  end
+
+  describe 'when I try to create a Keytab with a nonsensical type' do
+    it 'must raise an Error' do
+      -> { KerberosAuthenticator::Krb5::Keytab.new_with_name('DOJO:講道館') }.should.raise KerberosAuthenticator::Krb5::Error
+    end
+  end
+
+end
